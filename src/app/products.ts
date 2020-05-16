262701dd7151feb15f85135d5195c58fce3a3067
aluski97 <alanmorenoreyes@gmail.com>
2020-05-16 11:48:52 +0200

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,19 +1,44 @@
 export const products = [
   {
-    name: 'Xiaomi Mi Note 10',
-    price: 799,
+    name: 'Xiaomi Redmi Note 9',
+    price: 189,
     description: 'Lo mejor de Xiaomi con un precio inmejorable'
   },
   {
-    name: 'Huawei P30',
+    name: 'Huawei P30 Lite',
+    price: 299,
+    description: 'La mejor camara de un Smartphone en tus manos'
+  },
+  {
+    name: 'Huawei P30 Pro',
+    price: 799,
+    description: ''
+  },
+    {
+    name: 'Samsung Galaxy S8',
     price: 699,
     description: 'La mejor camara de un Smartphone en tus manos'
   },
   {
-    name: 'Xiaomi Redmi Note 9',
-    price: 299,
-    description: ''
-  }
+    name: 'Oppo A5 2020',
+    price: 209,
+    description: 'La mejor camara de un Smartphone en tus manos'
+  },
+  {
+    name: 'iPhone 11 Pro',
+    price: 1149,
+    description: 'La mejor camara de un Smartphone en tus manos'
+  },
+  {
+    name: 'Xiaomi Mi Note 10 Pro',
+    price: 399,
+    description: 'La mejor camara de un Smartphone en tus manos'
+  },
+  {
+    name: 'iPhone X',
+    price: 799,
+    description: 'La mejor camara de un Smartphone en tus manos'
+  },
 ];
 
 
