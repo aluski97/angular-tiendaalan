a76e157c8690ec1c4776383bb44d0ad636e11181
aluski97 <alanmorenoreyes@gmail.com>
2020-05-16 12:47:47 +0200

    2 files modified, 9 deleted

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,42 +3,49 @@ export const products = [
     name: 'Xiaomi Redmi Note 9',
     price: 189,
     description: 'Lo mejor de Xiaomi con un precio inmejorable',
-    img: '1.jpg'
+    route: '1.jpg'
   },
   {
     name: 'Huawei P30 Lite',
     price: 299,
-    description: 'La mejor camara de un Smartphone en tus manos'
+    description: 'La mejor camara de un Smartphone en tus manos',
+    route: '2.jpg'
   },
   {
     name: 'Huawei P30 Pro',
     price: 799,
-    description: ''
+    description: '',
+    route: '3.jpg'
   },
     {
     name: 'Samsung Galaxy S8',
     price: 699,
-    description: 'La mejor camara de un Smartphone en tus manos'
+    description: 'La mejor camara de un Smartphone en tus manos',
+    route:'4.jpg'
   },
   {
     name: 'Oppo A5 2020',
     price: 209,
-    description: 'La mejor camara de un Smartphone en tus manos'
+    description: 'La mejor camara de un Smartphone en tus manos',
+    route:'5.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1149,
-    description: 'La mejor camara de un Smartphone en tus manos'
+    description: 'La mejor camara de un Smartphone en tus manos',
+    route:'6.jpg'
   },
   {
     name: 'Xiaomi Mi Note 10 Pro',
     price: 399,
-    description: 'La mejor camara de un Smartphone en tus manos'
+    description: 'La mejor camara de un Smartphone en tus manos',
+    route:'7.jpg'
   },
   {
     name: 'iPhone X',
     price: 799,
-    description: 'La mejor camara de un Smartphone en tus manos'
+    description: 'La mejor camara de un Smartphone en tus manos',
+    route:'8.jpg'
   },
 ];
 
