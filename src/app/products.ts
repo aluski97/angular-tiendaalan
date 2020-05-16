069b01511844b6acfd46171644e1649b4f7e2cee
aluski97 <alanmorenoreyes@gmail.com>
2020-05-16 18:10:20 +0200

    10 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,21 +2,21 @@ export const products = [
   {
     name: 'Xiaomi Redmi Note 9',
     price: 189,
-    model: 'Azul Neón - 128GB + 6GB RAM',
-    description:'El móvil Xiaomi Redmi Note 9 en color blanco glaciar te ofrece rendimiento, diseño y potencia. Con pantalla de 6.67" FHD+ y procesador Qualcomm® Snapdragon™ 720G, tendrás la tecnología y los componentes de calidad que siempre te ofrece Xiaomi.',
+    model: 'Negro Azabache - 128GB + 6GB RAM',
+    description:'El móvil Xiaomi Redmi Note 9 en color negro azabache te ofrece rendimiento, diseño y potencia. Con pantalla de 6.67" FHD+ y procesador Qualcomm® Snapdragon™ 720G, tendrás la tecnología y los componentes de calidad que siempre te ofrece Xiaomi.',
     route: '1.jpg'
   },
   {
     name: 'Huawei P30 Lite',
     price: 299,
     model:'Blanco perla - 128GB + 4GB RAM',
-    description: 'Potente, resistente, funcional, casi del futuro. El móvil Huawei P30 Lite de color blanco pone a tu disposición una tecnología premium para que tengas una experiencia única. Un procesador potente, red 4G, buena seguridad, fotos casi profesionales, etc. Todo esto y más es lo que te ofrece este smartphone de Huawei.',
+    description: 'Potente, resistente, funcional, casi del futuro. El móvil Huawei P30 Lite de color blanco perla pone a tu disposición una tecnología premium para que tengas una experiencia única. Un procesador potente, red 4G, buena seguridad, fotos casi profesionales, etc. Todo esto y más es lo que te ofrece este smartphone de Huawei.',
     route: '2.jpg'
   },
   {
     name: 'Huawei P30 Pro',
     price: 799,
-    model:'Blanco azulado - 128GB + 8GB RAM',
+    model:'Rojo Anaranjado - 128GB + 8GB RAM',
     description: 'Llega la revolución. ¿Estás listo? El móvil Huawei P30 Pro llega para que disfrutes de unas prestaciones y un rendimiento de otro nivel. Su procesador de ocho núcleos se une a una serie de componentes que incluyen una triple cámara Leica para que saques tus mejores fotos y, en definitiva, tengas una experiencia única. Agárrate fuerte.',
     route: '3.jpg'
   },
@@ -30,15 +30,15 @@ export const products = [
   {
     name: 'Oppo A5 2020',
     price: 199,
-    model:'Negro - 64GB + 3GB RAM',
+    model:'Blanco Cristalizado - 64GB + 3GB RAM',
     description: 'Llega la revolución. ¿Estás listo? El móvil Oppo A5 llega para que disfrutes de unas prestaciones y un rendimiento de otro nivel. Su procesador se une a una serie de componentes que incluyen una doble cámara trasera para que saques tus mejores fotos y, en definitiva, tengas una experiencia única. Agárrate fuerte.',
     route:'5.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1149,
-    model:'Verde - 256GB + 4GB RAM',
-    description: 'Bienvenido a la experiencia del iPhone 11 Pro. Sumérgete en la OLED Super Retina XDR de 5.8 pulgadas del móvil Apple iPhone 11 Pro de 64 GB en color plata. Cuenta además con el chip A13 Bionic, con una inteligencia y potencia nunca vistas en un smartphone. Sin olvidar su revolucionario sistema de cámara dual y su reconocimiento facial aún más veloz. Todo lo que te gusta del iPhone, pero llevado al extremo.',
+    model:'Gris - 256GB + 4GB RAM',
+    description: 'Bienvenido a la experiencia del iPhone 11 Pro. Sumérgete en la OLED Super Retina XDR de 5.8 pulgadas del móvil Apple iPhone 11 Pro de 64 GB en color gris. Cuenta además con el chip A13 Bionic, con una inteligencia y potencia nunca vistas en un smartphone. Sin olvidar su revolucionario sistema de cámara dual y su reconocimiento facial aún más veloz. Todo lo que te gusta del iPhone, pero llevado al extremo.',
     route:'6.jpg'
   },
   {
@@ -51,7 +51,7 @@ export const products = [
   {
     name: 'iPhone X',
     price: 799,
-    model:'Plata - 64GB + 3GB RAM',
+    model:'Negro - 64GB + 3GB RAM',
     description: 'El Apple iPhone X es el móvil de gama top de Apple de 2017 y llega al mercado con una pantalla OLED panorámica con notch y un peso de 174 g. Tiene un diseño compacto con grosor de 7,7 mm con cuerpo unibody metálico y trasera de cristal.',
     route:'8.jpg'
   },
